160bb808bde2a43c63038c79a9496a05c07e6370
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-03-28 14:30:03 +0900

    todolist를 보여주는 화면에서 화살표 버튼을 눌렀을 때 원래의 todoList 화면을 보여주면서 해당 날짜의 데이터 값도 보여주는 기능 구현 (완료)

---
+++ todoList_/todoList_/Controller/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  todoList_
+//
+//  Created by 배지해 on 3/28/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
